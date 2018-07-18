657702c7d02837ff8381043ba33e00818f38a899
Domino <zhikhanastasya@gmail.com>
2018-07-18 09:21:13 +0300

    Add files

---
+++ Patterns/AmbientContext.cs
@@ -0,0 +1,29 @@
+﻿namespace Patterns
+{
+    public interface IWorker
+    {
+        void DoSmth();
+    }
+
+    internal class Worker : IWorker
+    {
+        public void DoSmth() { }
+    }
+
+    public class GlobalWorker
+    {
+        private static IWorker _someWorker = new Worker();
+        
+        public static IWorker Worker
+        {
+            get
+            {
+                return _someWorker;
+            }
+            internal set
+            {
+                _someWorker = value;
+            }
+        }
+    }
+}
---
+++ Patterns/Singleton.cs
@@ -0,0 +1,45 @@
+﻿namespace Drafts.Patterns
+{
+    public sealed class DoubleCheckedLockSingleton
+    {
+        private static volatile DoubleCheckedLockSingleton _instance;
+
+        private static readonly object _syncRoot = new object();
+
+        DoubleCheckedLockSingleton(){ }
+
+        public static DoubleCheckedLockSingleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new DoubleCheckedLockSingleton();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+
+    public sealed class LazyFieldInitSingleton
+    {
+        private LazyFieldInitSingleton() { }
+
+        public static LazyFieldInitSingleton Instance
+        {
+            get { return Nested._instance; }
+        }
+
+        private static class Nested
+        {
+            public static readonly LazyFieldInitSingleton _instance =
+            new LazyFieldInitSingleton();
+        }
+    }
+}
