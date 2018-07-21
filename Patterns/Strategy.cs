0183e63511e64434e3de452896b9b2769b8efe74
Domino <zhikhanastasya@gmail.com>
2018-07-21 22:46:56 +0300

    Add file

---
+++ Patterns/Strategy.cs
@@ -0,0 +1,23 @@
+﻿namespace Patterns
+{
+    // Encapsulates a specific behavior with the ability to replace it
+    public interface IStrategy
+    {
+        void Algorithm();
+    }
+
+    public class Context
+    {
+        public IStrategy ContextStrategy { get; set; }
+
+        public Context(IStrategy _strategy)
+        {
+            ContextStrategy = _strategy;
+        }
+
+        public void ExecuteAlgorithm()
+        {
+            ContextStrategy.Algorithm();
+        }
+    }
+}
