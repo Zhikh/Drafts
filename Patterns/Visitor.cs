bf2df509f605669edf0c940d942bbaf6f96e9084
Domino <zhikhanastasya@gmail.com>
2018-08-07 23:30:52 +0300

    Add (Patterns) visitor pattern

---
+++ Patterns/Visitor.cs
@@ -0,0 +1,35 @@
+﻿namespace Patterns
+{
+    public interface IVisitor
+    {
+        void Visit(ExceptionLogEntry exceptionLogEntry);
+    }
+
+    public abstract class LogEntry
+    {
+        public abstract void Accept(IVisitor logEntryVisitor);
+    }
+
+    public class ExceptionLogEntry : LogEntry
+    {
+        public override void Accept(IVisitor logEntryVisitor)
+        {
+            logEntryVisitor.Visit(this);
+        }
+    }
+
+    public class DatabaseLogSaver : IVisitor
+    {
+        public void SaveLogEntry(LogEntry logEntry)
+        {
+            logEntry.Accept(this);
+        }
+
+        void IVisitor.Visit(ExceptionLogEntry exceptionLogEntry)
+        {
+            SaveException(exceptionLogEntry);
+        }
+        
+        private void SaveException(ExceptionLogEntry logEntry) {}
+    }
+}
